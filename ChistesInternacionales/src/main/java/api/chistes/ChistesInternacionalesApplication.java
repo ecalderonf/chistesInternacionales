8037c70703b95ae5679ae24163bc02f26a806090
Edu <edu.calderon.es@gmail.com>
2025-07-15 13:29:10 +0200

    First Spring Boot

---
+++ ChistesInternacionales/src/main/java/api/chistes/ChistesInternacionalesApplication.java
@@ -0,0 +1,13 @@
+package api.chistes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChistesInternacionalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChistesInternacionalesApplication.class, args);
+	}
+
+}
---
+++ ChistesInternacionales/src/test/java/api/chistes/ChistesInternacionalesApplicationTests.java
@@ -0,0 +1,13 @@
+package api.chistes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChistesInternacionalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
